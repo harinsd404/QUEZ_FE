ee1edce42ee9b18c8c3cceba5a8bc3f6dbd51ede
harinsd404 <harinsd168@gmail.com>
2025-11-23 22:02:59 +0900

    feat::quiz 페이지 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '@/Page/home';
+import Quiz from '@/Page/quiz';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/quiz-page" element={<Quiz />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Page/quiz/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as _ from './style';
+
+const Quiz: React.FC = () => {
+  return <_.Container></_.Container>;
+};
+
+export default Quiz;
